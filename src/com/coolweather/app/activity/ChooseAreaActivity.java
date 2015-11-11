ee4d735527c44bbf2fb8127a018d99dbb9e7fce5
milk <1154073831@qq.com>
2015-11-11 18:36:23 +0800

    加入显示天气信息的功能.

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -6,8 +6,12 @@ import java.util.List;
 //import android.R;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
@@ -32,6 +36,11 @@ public class ChooseAreaActivity extends Activity {
 	public static final int LEVEL_CITY = 1;
 	public static final int LEVEL_COUNTY = 2;
 
+	/**
+	* 是否从WeatherActivity中跳转过来。
+	*/
+	private boolean isFromWeatherActivity;
+	
 	private ProgressDialog progressDialog;
 	private TextView titleText;
 	private ListView listView;
@@ -67,8 +76,18 @@ public class ChooseAreaActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		isFromWeatherActivity = getIntent().getBooleanExtra("from_weather_activity", false);
+		SharedPreferences prefs = PreferenceManager
+				.getDefaultSharedPreferences(this);
+		if (prefs.getBoolean("city_seleted", false)) {
+			Intent intent = new Intent(this, WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
+
 		listView = (ListView) findViewById(R.id.list_view);
 		titleText = (TextView) findViewById(R.id.title_text);
 		adapter = new ArrayAdapter<String>(this,
@@ -87,10 +106,17 @@ public class ChooseAreaActivity extends Activity {
 				} else if (currentLevel == LEVEL_CITY) {
 					selectedCity = cityList.get(index);
 					queryCounties();
+				} else if (currentLevel == LEVEL_COUNTY) {
+					String countyCode = countyList.get(index).getCountCode();
+					Intent intent = new Intent(ChooseAreaActivity.this,
+							WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 		});
-		queryProvinces();
+		queryProvinces(); // 加载省级数据
 	}
 
 	/*
@@ -98,18 +124,18 @@ public class ChooseAreaActivity extends Activity {
 	 */
 	private void queryProvinces() {
 		// TODO Auto-generated method stub
-		provinceList=coolWeatherDB.loadProvinces();
-		if(provinceList.size()>0){
+		provinceList = coolWeatherDB.loadProvinces();
+		if (provinceList.size() > 0) {
 			dataList.clear();
-			for(Province province:provinceList){
+			for (Province province : provinceList) {
 				dataList.add(province.getProvinceName());
 			}
 			adapter.notifyDataSetChanged();
 			listView.setSelection(0);
 			titleText.setText("中国");
-			currentLevel=LEVEL_PROVINCE;
-		}else{
-			queryFromServer(null,"province");
+			currentLevel = LEVEL_PROVINCE;
+		} else {
+			queryFromServer(null, "province");
 		}
 	}
 
@@ -223,47 +249,35 @@ public class ChooseAreaActivity extends Activity {
 
 	private void showProgressDialog() {
 		// TODO Auto-generated method stub
-		if(progressDialog==null){
-			progressDialog=new ProgressDialog(this);
+		if (progressDialog == null) {
+			progressDialog = new ProgressDialog(this);
 			progressDialog.setMessage("正在加载...");
 			progressDialog.setCanceledOnTouchOutside(false);
 		}
 		progressDialog.show();
 	}
-	
-	private void closeProgressDialog(){
-		if(progressDialog!=null){
+
+	private void closeProgressDialog() {
+		if (progressDialog != null) {
 			progressDialog.dismiss();
 		}
 	}
-	
+
 	/*
 	 * 捕获Back按键，根据当前的级别来判断，此时应该返回市列表、省列表，还是直接退出
 	 */
-	public void onBackPressed(){
-		if(currentLevel==LEVEL_COUNTY){
+	public void onBackPressed() {
+		if (currentLevel == LEVEL_COUNTY) {
 			queryCities();
-		}else if(currentLevel==LEVEL_CITY){
+		} else if (currentLevel == LEVEL_CITY) {
 			queryProvinces();
-		}else{
+		} else {
+			if (isFromWeatherActivity) {
+				Intent intent = new Intent(this, WeatherActivity.class);
+				startActivity(intent);
+			}
 			finish();
 		}
+		
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
